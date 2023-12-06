863132a6ea8de1cf513d8e7b2473791285ff3488
lucas_broda <lucas_broda@ens.univ-artois.fr>
2023-12-06 19:36:26 +0100

    + finished header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div className='text-white'>
-        <h1>Hello, I'm Lucas Broda</h1>
+    <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className="col-span-7 place-self-center text-center sm:text-left ">
+            <h1 className=' text-white mb-8 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                Hello,<br/> I'm Lucas Broda
+            </h1>
+            <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6 '>
+                I'm a 20-year-old French student in my 3rd year<br/>
+                of Computer Science degree at the IUT in Lens.<br/>
+                Bellow, you will find a link to see/download my CV :<br/> 
+            </p>
+            <div className="">
+                <button className="px-6 py-3 bg-white text-black rounded-full  hover:bg-slate-200">
+                    My CV
+                </button>
+               
+            </div>
+        </div>
+
+        <div className="col-span-5 place-self-center mt-20 lg:mt-0 sm: ml-8">
+            <div className="relative w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
+                <Image className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/LucasB.jpg" alt="Picture of my self" width={300} height={500}/>
+            </div>
+        </div>
     </div>
   )
 };
