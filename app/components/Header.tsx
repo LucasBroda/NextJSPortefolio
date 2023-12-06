1df3d4edd99ab19fc6aed20508051b2a2adb7f41
lucas_broda <lucas_broda@ens.univ-artois.fr>
2023-12-06 17:39:45 +0100

    + header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className='text-white'>
+        <h1>Hello, I'm Lucas Broda</h1>
+    </div>
+  )
+};
+
+export default Header;
