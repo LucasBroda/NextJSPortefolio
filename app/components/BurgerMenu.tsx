f3bad21b96c5e4d12fb53db0455154ae2a3f0c49
lucas_broda <lucas_broda@ens.univ-artois.fr>
2023-12-29 10:20:40 +0100

    fix(BurgerMenu): added an interface to fix the links.map problem

--- app/components/BurgerMenu.tsx
+++ app/components/BurgerMenu.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import NavbarLinks from "./NavbarLinks";
 
-const BurgerMenu = ( links ) => {
+interface Link {
+    href: string;
+    title: string;
+}
+
+const BurgerMenu = ({ links }: { links: Link[] }) => {
     return (
         <ul className="flex flex-col py-4 items-center">
             {links.map((link, index) => (
                 <li key={index}>
-                    <NavbarLinks  href={link.href} title={link.title} />
+                    <NavbarLinks href={link.href} title={link.title} />
                 </li>
             ))}
         </ul>
-    )
-}
+    );
+};
 
 export default BurgerMenu;
