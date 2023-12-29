13f16fe65fc57528e12c7e048878929a70ef320a
lucas_broda <lucas_broda@ens.univ-artois.fr>
2023-12-29 17:58:36 +0100

    feat(Contact): added my email :D

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,6 +2,7 @@ const Contact = () => {
     return (
         <div id="contact">
             <h1 className="text-center text-4xl font-bold  mt-28 mb-8">Contact</h1>
+            <p className="text-center text-xl font-bold">If you want to contact me, just send an email to this adress: lucas.broda1280@gmail.com</p>
         </div>
     )
 }
